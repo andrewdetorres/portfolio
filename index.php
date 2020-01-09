e8ff92bbcc999d68787e717142fc60222faade54
Andrew de torres <andrew.de.torres@hotmail.co.uk>
2020-01-09 16:28:14 +0000

    initialise portfolio project

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Andrew De Torres | Full Stack Web Developer | Vancouver</title>
+
+  <!-- Meta Tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="images/favicon.ico" />
+  <meta
+    name="description"
+    content="Full stack web developer currently residing in the Vancouver area. Skills include Wordpress, Drupal, ReactJS, PHP, NodeJS."
+    />
+  <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="apple-touch-icon" href="images/iphone_icon.png" />
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="assets/css/style.css"/>
+
+  <!-- Google Font for logo   -->
+  <link href="assets/fonts/fontawesome/css/all.min.css" rel="stylesheet" />
+</head>
+<body>
+
+  <?php require_once('templates/navigation.html'); ?>
+
+
+</body>
+<footer>
+    <!-- Bootstrap Jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</footer>
+</html>
